96a43aaae30d0266d1f65247e8365d198a2c5509
csmailis <22669824+csmailis@users.noreply.github.com>
2022-05-26 05:18:02 -0500

    Add files via upload

---
+++ install_nvidia_smi_icons.sh
@@ -0,0 +1,3 @@
+ cp 4518899_nvidia_icon.png ~/
+ cp nvidia_smi_gnome.sh ~/
+ cp nvidia-smi.desktop ~/.local/share/applications/
---
+++ nvidia_smi_gnome.sh
@@ -0,0 +1,1 @@
+gnome-terminal -x sh -c "watch -n0.1 nvidia-smi; exec bash"
