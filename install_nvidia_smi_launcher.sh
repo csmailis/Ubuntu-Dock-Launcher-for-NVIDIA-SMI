219c4c6f1828d1bd1acf705a44c69a2e30bd9ba9
csmailis <22669824+csmailis@users.noreply.github.com>
2022-05-26 05:56:39 -0500

    Add files via upload

---
+++ install_nvidia_smi_launcher.sh
@@ -0,0 +1,3 @@
+ cp 4518899_nvidia_icon.png /usr/share/icons/
+ cp nvidia_smi_gnome.sh /usr/share/
+ cp nvidia-smi.desktop ~/.local/share/applications/
---
+++ uninstal_nvidia_smi_launcher.sh
@@ -0,0 +1,3 @@
+ rm  /usr/share/icons/4518899_nvidia_icon.png
+ rm  /usr/share/nvidia_smi_gnome.sh
+ rm  ~/.local/share/applications/nvidia-smi.desktop
